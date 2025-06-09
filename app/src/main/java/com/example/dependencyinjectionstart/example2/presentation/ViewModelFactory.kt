3f069fe856d0e7c8c61bcd81e263c364c1df3b46
sumin93 <suminfx@mail.ru>
2025-06-09 22:43:32 +0300

    Upgrade dependencies

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
@@ -8,7 +8,7 @@ class ViewModelFactory @Inject constructor(
     private val viewModels: @JvmSuppressWildcards Map<String, ViewModel>
 ) : ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return viewModels[modelClass.simpleName] as T
     }
 }
